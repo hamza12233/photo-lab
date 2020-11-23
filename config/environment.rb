c6eaa8a052c4384b3ca0ac85ac21ae957c62b107
hamza <hafizhamzasaleem4@gmail.com>
2020-11-23 14:03:12 +0500

    email action set

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'apikey',
+    :password => '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',
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
