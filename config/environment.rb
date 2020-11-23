d9ffbc972954f387015ae778348e14efcb1a771f
hamza <hafizhamzasaleem4@gmail.com>
2020-11-23 18:29:28 +0500

    set

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,12 @@ Rails.application.initialize!
 
 
 ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
+    :from           => 'hafizhamzasaleem4@gmail.com',
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'https://photo-lab-new.herokuapp.com',
     :enable_starttls_auto => true
-}
+} 
