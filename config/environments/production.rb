f54a3621c95bdfcb1421de2428c56927e09895ed
hamza <hafizhamzasaleem4@gmail.com>
2020-11-23 14:12:57 +0500

    set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "photo-lab-new.herokuapp.com", :protocol =>'https' }
+  config.action_mailer.default_url_options = { :host => "https://photo-lab-new.herokuapp.com/" }
 
 
   # Full error reports are disabled and caching is turned on.
